ec8d5e6a3d852a8c7a75d6b9c454148d0effe4a6
geek <elaminiaymane03@gmail.com>
2024-01-30 15:26:06 +0100

    change to the other branch

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,8 +1,31 @@
 <x-layout>
     <main class="container mx-auto mb-32">
         <form method="POST" action="/posts/store" enctype="multipart/form-data">
-            <input type="file" name="images[]" multiple>
+            <input id="imageUpload" type="file" name="images[]" multiple>
+            @if(request("images"))
+                @php echo "here" ;@endphp
+            @endif
+            <div id="imagePreview" class="imagePreview"></div>
+
             <button type="submit">Upload</button>
         </form>
+
+        <script>
+            document.getElementById('imageUpload').addEventListener('change', function () {
+                let preview = document.getElementById('imagePreview');
+                let files = this.files;
+                for (let i = 0; i < files.length; i++) {
+                    let file = files[i];
+                    let reader = new FileReader();
+                    reader.onload = function (event) {
+                        let img = document.createElement('img');
+                        img.src = event.target.result;
+                        <img src="" alt="" className="style maxWidt"/>h = '200px'; // Set maximum width for preview image
+                        preview.appendChild(img);
+                    }
+                    reader.readAsDataURL(file);
+                }
+            });
+        </script>
     </main>
 </x-layout>
